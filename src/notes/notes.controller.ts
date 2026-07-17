dcbc87829291ba9e75d184e71882e7af457d3bd7
Ismatul Islam Pranto <prantokhaan@glmail.com>
2026-07-18 03:23:54 +0600

    crud

---
+++ src/notes/notes.controller.ts
@@ -0,0 +1,88 @@
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Put, Query, StreamableFile } from '@nestjs/common';
+import { NotesService } from './notes.service';
+import type { Note } from './notes.interface';
+import { createReadStream } from 'fs';
+import { join } from 'path';
+
+@Controller({
+    path: "notes",
+    version: "1"
+})
+export class NotesController {
+    constructor(private readonly notesService: NotesService) {}
+
+    @Get()
+    filterCategory(
+        @Query('category') category: string
+    ): Note[] {
+        if (category) return this.notesService.filterCategory(category);
+        return this.notesService.getAllNotes();
+    }
+
+    @Post()
+    createNote(
+        @Body() body: {title: string, content: string, category: string, isPinned: boolean}
+    ): Note {
+        return this.notesService.createNote(body.title, body.content, body.category, body.isPinned);
+    }
+
+    @Get("search")
+    searchNotes(
+        @Query('keyword') keyword: string
+    ): Note[] {
+        return this.notesService.searchNotes(keyword);
+    }
+
+    @Get("pinned")
+    pinnedNotes(): Note[] {
+        return this.notesService.pinnedNotes();
+    }
+
+    @Get('export')
+    noteDownload() {
+        const filepath = join(process.cwd(), "test.txt");
+        const file = createReadStream(filepath);
+
+        return new StreamableFile(file, {
+            type: "text/plain",
+            disposition: 'attachment; filename="test.txt'
+        });
+    }
+
+    @Get(":id")
+    getNoteById(
+        @Param('id', ParseIntPipe) id: number,
+    ): Note {
+        return this.notesService.getNoteById(id);
+    }
+
+    @Put(":id")
+    updateNote(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() body: {title: string, content: string, category: string, isPinned: boolean}
+    ): Note {
+        return this.notesService.updateNote(id, body.title, body.content, body.category, body.isPinned);
+    }
+
+    @Delete(":id")
+    deleteNote(
+        @Param('id', ParseIntPipe) id: number
+    ): Note {
+        return this.notesService.deleteNote(id);
+    }
+
+    
+
+    
+
+    
+
+    @Patch(':id/pin')
+    updatePin(
+        @Param('id', ParseIntPipe) id: number,
+    ): Note {
+        return this.notesService.updatePin(id);
+    }
+
+    
+}
---
+++ src/notes/notes.service.ts
@@ -0,0 +1,116 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import type { Note } from './notes.interface';
+
+
+@Injectable()
+export class NotesService {
+    private notes: Note[] = [
+        {
+            id: 1,
+            title: "NestJS",
+            content: "Learning NestJS",
+            category: "programming",
+            isPinned: false
+        },
+        {
+            id: 2,
+            title: "TypeScript",
+            content: "Learning TypeScript",
+            category: "programming",
+            isPinned: true
+        },
+        {
+            id: 3,
+            title: "Love",
+            content: "Love is Dangerous",
+            category: "personal",
+            isPinned: true
+        },
+        {
+            id: 4,
+            title: "Hate",
+            content: "Hate is Dangerous",
+            category: "personal",
+            isPinned: false
+        }
+    ];
+    private nextId: number = 5;
+
+
+    getAllNotes(): Note[]{
+        return this.notes;
+    }
+
+    createNote(title: string, content: string, category: string, isPinned: boolean): Note{
+        const newNote: Note = {
+            id: this.nextId++,
+            title,
+            content,
+            category,
+            isPinned
+        }
+
+        this.notes.push(newNote);
+
+        return newNote;
+    }
+
+    getNoteById(id: number): Note{
+        const note = this.notes.find((item) => item.id === id);
+        if(!note){
+            throw new NotFoundException(`Note with ID ${id} not found.`);
+        }
+
+        return note;
+    }
+
+    updateNote(id: number, title?: string, content?: string, category?: string, isPinned?: boolean): Note {
+        const note = this.getNoteById(id);
+
+        if(title !== undefined) note.title = title;
+        if(content !== undefined) note.content = content;
+        if(category !== undefined) note.category = category;
+        if(isPinned !== undefined) note.isPinned = isPinned;
+
+        return note;
+    }
+
+    deleteNote(id: number): Note {
+        const note = this.getNoteById(id);
+
+        this.notes = this.notes.filter(
+            (ex) => ex.id !== id
+        );
+
+        return note;
+    }
+
+    searchNotes(keyword: string): Note[] {
+        const normalKey = keyword.toLowerCase();
+
+        return this.notes.filter(
+            (ex) => ex.content.toLowerCase().includes(normalKey)
+        )
+    }
+
+    filterCategory(category: string): Note[] {
+        return this.notes.filter(
+            (ex) => ex.category === category
+        )
+    }
+
+    pinnedNotes(): Note[] {
+        return this.notes.filter(
+            (ex) => ex.isPinned
+        )
+    }
+
+    updatePin(id: number): Note {
+        const note = this.getNoteById(id);
+
+        note.isPinned = !note.isPinned;
+
+        return note;
+    }
+
+}
